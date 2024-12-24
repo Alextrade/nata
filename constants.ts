a43effc8f462769ec7f0140820d8678fb3c7fc97
Alextrade <40890651+Alextrade@users.noreply.github.com>
2024-12-24 17:33:04 +0200

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+
+// Загружаем переменные из .env файла
+dotenv.config();
+
+export const PORT = Number(process.env.PORT) || 3306;  // Порт по умолчанию 3306
+export const APP_URL = String(process.env.APP_URL) || 'http://localhost';
+export const TELEGRAM_TOKEN = String(process.env.TELEGRAM_TOKEN) || '';
---
+++ index.ts
@@ -0,0 +1,46 @@
+import express, { Application } from "express";
+import { Telegraf } from "telegraf";
+import { APP_URL, PORT, TELEGRAM_TOKEN } from "./constants"; // Импорт из твоего файла с константами
+
+// Используем процесс для динамического порта или по умолчанию 3000
+const port = process.env.PORT || 3000;
+const app: Application = express();
+
+// Настройка статических файлов (если у тебя есть папка 'static' с медиа)
+app.use(express.static("static"));
+app.use(express.json());
+
+// Главный маршрут
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+// Создание Telegram-бота с использованием токена (здесь заменим на переменную из твоих констант)
+const bot = new Telegraf(TELEGRAM_TOKEN);
+
+// Команда "start" для бота
+bot.command("start", (ctx) => {
+  return ctx.reply("Play RumVall", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Play Game",
+            web_app: { url: `${APP_URL}` },
+          },
+        ],
+      ],
+    },
+  });
+});
+
+// Запуск бота
+bot.launch();
+
+// Экспортируем приложение для использования на сервере
+export default app;
